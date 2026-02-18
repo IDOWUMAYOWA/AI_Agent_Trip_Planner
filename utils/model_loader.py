c154ac394ac7bb303fcc8864168a632564d3187f
IDOWUMAYOWA <sangotadeidowu@yahoo.com>
2026-02-18 17:37:25 +0100

    streamlit structure added

---
+++ utils/model_loader.py
@@ -0,0 +1,63 @@
+import os
+from dotenv import load_dotenv
+from typing import Literal, Optional, Any
+from pydantic import BaseModel, Field
+from utils.config_loader import load_config
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+
+class ConfigManager:
+    def __init__(self):
+        
+        self.config = load_config()
+        print(f"Loaded configuration: {load_config()}")
+    
+    def __getitem__(self, key):
+        return self.config[key]
+
+
+class ModelLoader(BaseModel):
+    model_provider: Literal["groq", "openai", "anthropic"] = "groq"
+    config: Optional[ConfigManager] = Field(default=None, exclude=True)
+
+    def model_post_init(self, __context: Any) -> None:
+        self.config = ConfigManager()
+    
+    class Config:
+        arbitrary_types_allowed = True
+    
+    def load_llm(self):
+        """
+        Load and return the LLM model.
+        """
+        print("LLM loading...")
+        print(f"Loading model from provider: {self.model_provider}")
+        
+        if self.model_provider == "groq":
+            print("Loading LLM from Groq..............")
+            groq_api_key = os.getenv("GROQ_API_KEY")
+            model_name = self.config["llm"]["groq"]["model_name"]
+            llm = ChatGroq(model=model_name, api_key=groq_api_key)
+        
+        elif self.model_provider == "openai":
+            print("Loading LLM from OpenAI..............")
+            openai_api_key = os.getenv("OPENAI_API_KEY")
+            model_name = self.config["llm"]["openai"]["model_name"]
+            llm = ChatOpenAI(model_name=model_name, api_key=openai_api_key)
+        
+        elif self.model_provider == "anthropic":
+            print("Loading LLM from Claude..............")
+            anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+            model_name = self.config["llm"]["claude"]["model_name"]
+            
+            llm = ChatAnthropic(
+                model=model_name,  
+                anthropic_api_key=anthropic_api_key  
+            )
+        
+        else:
+            raise ValueError(f"Unknown provider: {self.model_provider}")
+        
+        return llm
